c94d27b9a248000a2293e0df5ea2bdc1a80c6746
gartland98 <gartland223@gmail.com>
2020-08-28 21:00:14 +0900

    changed the HTML for the site

--- mytestsite/mytestsite/urls.py
+++ mytestsite/mytestsite/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import path, include
 
 urlpatterns = [
     #url('^', include('coursework.urls')),
-    url('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('coursework.urls')),
 ]
